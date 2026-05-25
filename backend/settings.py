87db67445bea79fc48669f05e43afbd3543dca0c
nasho24 <ignacio.sanhueza14@inacapmail.cl>
2026-05-25 01:25:10 -0400

    Web Maestro Ventanero lista para desplegar

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['nacho24.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['Nacho24.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
